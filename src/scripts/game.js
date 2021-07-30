cfcec581377b17b6c1b7ebd47b3f58adf6c3a924
Justin Ernst <jernst766@gmail.com>
2021-07-30 08:32:00 -0500

    fix styling and color scheme

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,49 +1,8 @@
 import { lizard } from '../assets/images/lizard'
-// import Canvas from './Canvas';
 import { cute } from '../assets/images/cute'
-
-// let img = new Image();
-
-// img.crossOrigin = "Anonymous"
-// img.setAttribute(`src`, `data:image/jpg;base65, ${cute}`);
-// img.src = lizard;
-// img.addEventListener('load', Canvas.drawCards, false);
-
-// const canvas = document.getElementById("puzzle");
-// const canvas = document.createElement('canvas');
-
-// const ctx = canvas.getContext("2d");
-// let boardSize = canvas.width;
-// let cardSize = boardSize / 3;
-// let cardsArray;
-// // let emptyCard = 8;
-// let isSolved = false;
-
-// let emptyCard = {
-//   x: null,
-//   y: null
-// }
-
-// let mouse = {
-//   x: null,
-//   y: null
-// }
-
-
 
 
 export const findClick = function(e) {
-  // debugger
-  // let canvas = document.getElementById('puzzle');
-  // let rect = canvas.getBoundingClientRect();
-
-  
-    // let cardSize = document.getElementById('puzzle').width / 3;
-    
-    
-    // window.puzzle.width = width;
-    // window.puzzle.height = height;
-    
 
     let rect = e.target.getBoundingClientRect();
     let width = rect.width;
@@ -59,21 +18,9 @@ export const findClick = function(e) {
     }
     debugger
     console.log(`clickLoc: ${clickLoc}`);
-    // console.log(`[e.clientX, e.clientY]: ${[e.clientX, e.clientY]}`);
-    // console.log(`cardSize: ${cardSize}`);
+ 
     console.log(`[rect.left, rect.top]: ${[e.offsetX, e.offsetY]}`);
 
     debugger
     window.puzzle.switchCards(clickLoc);
 }
-// //let's set an event listener on the canvas
-// canvas.onmousemove = function(event) {
-//   mouse.x = Math.floor((event.pageX - event.offsetLeft) / cardSize);
-//   mouse.y = Math.floor((event.pageY - event.offsetTop) / cardSize);
-// }
-
-
-
-
-
-// setBoard();
